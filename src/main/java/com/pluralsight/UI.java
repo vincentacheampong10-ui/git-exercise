f5a785c8f9e3fbb5fe6935a45373d223028d72d8
Vincentacheampong <Vincentacheampong10@gmail.com>
2025-11-26 12:03:46 -0500

    calculator method added

--- src/main/java/com/pluralsight/UI.java
+++ src/main/java/com/pluralsight/UI.java
@@ -5,14 +5,33 @@ import java.util.Scanner;
 public class UI {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        boolean run = true;
+
+        while (run) {
+            System.out.print("What is you name?:");
+            String name = scanner.nextLine();
+
+            System.out.print("What is your age?:");
+            int age = scanner.nextInt();
+
+            System.out.println("Hello " + name + " ,You are " + age + " years old.");
+           run = false;
+           scanner.close();
+        }
+    }
+public void calculator (Scanner scanner) {
+boolean calculate = true;
+
+    while (calculate) {
+        System.out.println(""" 
+               1) Add
+               2) Multiply
+               3) Divide
+               4) Subtract
+          chose and option:
+                """);
 
 
-        System.out.print("What is you name?:");
-        String name = scanner.nextLine();
-
-        System.out.print("What is your age?:");
-        int age = scanner.nextInt();
-
-        System.out.println("Hello "+name+ " ,You are "+ age +" years old.");
+    }
     }
 }
