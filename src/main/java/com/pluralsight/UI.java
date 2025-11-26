dd04538605083785a9b945d6fec416acf6e16e23
Vincentacheampong <Vincentacheampong10@gmail.com>
2025-11-26 11:51:07 -0500

    UI

--- src/main/java/com/pluralsight/UI.java
+++ src/main/java/com/pluralsight/UI.java
@@ -2,7 +2,7 @@ package com.pluralsight;
 
 import java.util.Scanner;
 
-public class Output {
+public class UI {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
